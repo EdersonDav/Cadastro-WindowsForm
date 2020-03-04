501908c3f75c415b44763ebbc39e9397d4d5e681
SilvaEderson <silva.edersonqueiroz@gmail.com>
2020-03-04 12:48:27 -0300

    commit01
    commit inicial do arquivo

---
+++ ConhecimentosWindowsForms/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace ConhecimentosWindowsForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textNome = new System.Windows.Forms.TextBox();
+            this.txtEstado = new System.Windows.Forms.TextBox();
+            this.txtCidade = new System.Windows.Forms.TextBox();
+            this.txtSobrenome = new System.Windows.Forms.TextBox();
+            this.dateNascimento = new System.Windows.Forms.DateTimePicker();
+            this.btnMasculino = new System.Windows.Forms.RadioButton();
+            this.btnFeminino = new System.Windows.Forms.RadioButton();
+            this.lstSaida = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Sexo = new System.Windows.Forms.Label();
+            this.Cadastrar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textNome
+            // 
+            this.textNome.Location = new System.Drawing.Point(300, 12);
+            this.textNome.Name = "textNome";
+            this.textNome.Size = new System.Drawing.Size(238, 20);
+            this.textNome.TabIndex = 0;
+            // 
+            // txtEstado
+            // 
+            this.txtEstado.Location = new System.Drawing.Point(300, 92);
+            this.txtEstado.Name = "txtEstado";
+            this.txtEstado.Size = new System.Drawing.Size(238, 20);
+            this.txtEstado.TabIndex = 3;
+            // 
+            // txtCidade
+            // 
+            this.txtCidade.Location = new System.Drawing.Point(300, 132);
+            this.txtCidade.Name = "txtCidade";
+            this.txtCidade.Size = new System.Drawing.Size(238, 20);
+            this.txtCidade.TabIndex = 4;
+            // 
+            // txtSobrenome
+            // 
+            this.txtSobrenome.Location = new System.Drawing.Point(300, 49);
+            this.txtSobrenome.Name = "txtSobrenome";
+            this.txtSobrenome.Size = new System.Drawing.Size(238, 20);
+            this.txtSobrenome.TabIndex = 5;
+            // 
+            // dateNascimento
+            // 
+            this.dateNascimento.Location = new System.Drawing.Point(300, 178);
+            this.dateNascimento.Name = "dateNascimento";
+            this.dateNascimento.Size = new System.Drawing.Size(238, 20);
+            this.dateNascimento.TabIndex = 6;
+            // 
+            // btnMasculino
+            // 
+            this.btnMasculino.AutoSize = true;
+            this.btnMasculino.Location = new System.Drawing.Point(300, 219);
+            this.btnMasculino.Name = "btnMasculino";
+            this.btnMasculino.Size = new System.Drawing.Size(73, 17);
+            this.btnMasculino.TabIndex = 7;
+            this.btnMasculino.TabStop = true;
+            this.btnMasculino.Text = "Masculino";
+            this.btnMasculino.UseVisualStyleBackColor = true;
+            // 
+            // btnFeminino
+            // 
+            this.btnFeminino.AutoSize = true;
+            this.btnFeminino.Location = new System.Drawing.Point(453, 219);
+            this.btnFeminino.Name = "btnFeminino";
+            this.btnFeminino.Size = new System.Drawing.Size(67, 17);
+            this.btnFeminino.TabIndex = 8;
+            this.btnFeminino.TabStop = true;
+            this.btnFeminino.Text = "Feminino";
+            this.btnFeminino.UseVisualStyleBackColor = true;
+            // 
+            // lstSaida
+            // 
+            this.lstSaida.FormattingEnabled = true;
+            this.lstSaida.Location = new System.Drawing.Point(248, 270);
+            this.lstSaida.Name = "lstSaida";
+            this.lstSaida.Size = new System.Drawing.Size(353, 160);
+            this.lstSaida.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(225, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Nome:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(204, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Sobrenome:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(220, 99);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Cidade:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(220, 139);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Estado:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(176, 184);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(92, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Data Nascimento:";
+            // 
+            // Sexo
+            // 
+            this.Sexo.AutoSize = true;
+            this.Sexo.Location = new System.Drawing.Point(234, 223);
+            this.Sexo.Name = "Sexo";
+            this.Sexo.Size = new System.Drawing.Size(34, 13);
+            this.Sexo.TabIndex = 15;
+            this.Sexo.Text = "Sexo:";
+            this.Sexo.Click += new System.EventHandler(this.Sexo_Click);
+            // 
+            // Cadastrar
+            // 
+            this.Cadastrar.Location = new System.Drawing.Point(641, 322);
+            this.Cadastrar.Name = "Cadastrar";
+            this.Cadastrar.Size = new System.Drawing.Size(107, 54);
+            this.Cadastrar.TabIndex = 16;
+            this.Cadastrar.Text = "Cadastrar";
+            this.Cadastrar.UseVisualStyleBackColor = true;
+            this.Cadastrar.Click += new System.EventHandler(this.Cadastrar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Cadastrar);
+            this.Controls.Add(this.Sexo);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lstSaida);
+            this.Controls.Add(this.btnFeminino);
+            this.Controls.Add(this.btnMasculino);
+            this.Controls.Add(this.dateNascimento);
+            this.Controls.Add(this.txtSobrenome);
+            this.Controls.Add(this.txtCidade);
+            this.Controls.Add(this.txtEstado);
+            this.Controls.Add(this.textNome);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textNome;
+        private System.Windows.Forms.TextBox txtEstado;
+        private System.Windows.Forms.TextBox txtCidade;
+        private System.Windows.Forms.TextBox txtSobrenome;
+        private System.Windows.Forms.DateTimePicker dateNascimento;
+        private System.Windows.Forms.RadioButton btnMasculino;
+        private System.Windows.Forms.RadioButton btnFeminino;
+        private System.Windows.Forms.ListBox lstSaida;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label Sexo;
+        private System.Windows.Forms.Button Cadastrar;
+    }
+}
+
---
+++ ConhecimentosWindowsForms/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConhecimentosWindowsForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Sexo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Cadastrar_Click(object sender, EventArgs e)
+        {
+            //Limpar box para novo cadastro
+            lstSaida.Items.Clear();
+
+            //Converter a data e fazer o calculo da idade
+            DateTime data = Convert.ToDateTime(dateNascimento.Value);
+            int anos = DateTime.Today.Year - data.Year;
+
+            string sexo = "", nome = textNome.Text, sobrenome = txtSobrenome.Text, estado = txtCidade.Text, cidade = txtEstado.Text;
+
+            //Atribuindo o sexo
+            if (btnFeminino.Checked)
+            {
+
+                sexo = "Feminino";
+            }
+            else
+            {
+                if (btnMasculino.Checked)
+                {
+
+                    sexo = "Masculino";
+                }
+
+
+            }
+
+            //Validando os campos em branco 
+            if (nome == "" || sobrenome == "" || estado == "" || cidade == "" || sexo == "")
+            {
+
+                if (nome == "")
+                {
+                    lstSaida.Items.Add("Por favor preencher o campo 'Nome'");
+
+                }
+
+                if (sobrenome == "")
+                {
+                    lstSaida.Items.Add("Por favor preencher o campo 'Sobrenome'");
+
+                }
+
+                if (estado == "")
+                {
+                    lstSaida.Items.Add("Por favor preencher o campo 'Estado'");
+
+                }
+
+                if (cidade == "")
+                {
+                    lstSaida.Items.Add("Por favor preencher o campo 'Cidade'");
+
+                }
+
+                if (sexo == "")
+                {
+                    lstSaida.Items.Add("Por favor preencher o campo 'Sexo'");
+
+                }
+
+            }
+
+            //Se tudo estiver correto mostrar o cadastro
+            else
+            {
+                lstSaida.Items.Add("Você realizou o cadastro de:");
+                lstSaida.Items.Add($"Nome: {nome} {sobrenome}");
+                lstSaida.Items.Add($"Mora em: {cidade}, {estado}");
+                lstSaida.Items.Add($"Idade: {anos} anos");
+                lstSaida.Items.Add($"Sexo: {sexo} ");
+
+                //Banco de dados 
+                //Conexao SQL
+                SqlConnection con = new SqlConnection("Data Source = ; Initial Catalog = EmployeeDB; Integrated Security = True");
+
+                //Insert SQL
+                string sql = $"Insert into Cadastro(Nome,Sobrenome,Cidade,Estado,Idade,Sexo) values(@nome,@sobrenome,@cidade,@estado,@idade,@sexo)";
+
+
+                try
+                {
+                    //Criando o objeto para passar a string e fazer a conexão
+                    SqlCommand entrada = new SqlCommand(sql, con);
+
+                    //Enviando os parametros
+                    entrada.Parameters.Add(new SqlParameter("@nome", nome));
+                    entrada.Parameters.Add(new SqlParameter("@sobrenome", sobrenome));
+                    entrada.Parameters.Add(new SqlParameter("@cidade", cidade));
+                    entrada.Parameters.Add(new SqlParameter("@estado", estado));
+                    entrada.Parameters.Add(new SqlParameter("@idade", anos));
+                    entrada.Parameters.Add(new SqlParameter("@sexo", sexo));
+
+                    //abrindo a conexão
+                    con.Open();
+
+                    //Executar comando 
+                    entrada.ExecuteNonQuery();
+
+                    //Fechando a conexão
+                    con.Close();
+
+                    lstSaida.Items.Add("Usuário cadastrado no banco");
+                }
+                catch (SqlException ex)
+                {
+                    lstSaida.Items.Add(ex);
+                }
+                finally
+                {
+                    con.Close();
+                }
+
+                //Limpando o formulário para novo cadastro
+                btnFeminino.Checked = false;
+                btnMasculino.Checked = false;
+                textNome.Text = "";
+                txtEstado.Text = "";
+                txtCidade.Text = "";
+                txtSobrenome.Text = "";
+                dateNascimento.Value = DateTime.Today;
+            }
+
+        }
+    }
+}
